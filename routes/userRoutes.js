c0c0a5974ac3d594452d67ea950649612e1df228
Paulo Calderon <paulojosecalderon@gmail.com>
2023-11-23 11:24:36 +0800

    User Routes and their logic

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,50 @@
 const express = require('express')
 const router = express.Router();
 
+//UUID package for creating unique user ID's
+const { v4: uuidv4 } = require('uuid');
+
 //Array container acting as database for user input
 let users=[];
 
 router.route('/').get((req,res)=>{
     res.send(users)
 }).post((req,res)=>{
-    users.push(req.body);
-    res.send(`User ${req.body.firstName} has been posted!`)
+    let user = req.body;
+    users.push({
+        ...user,
+        id: uuidv4()
+    })
+
+    res.send(`User ${user.firstName} has been posted!`)
 })
+
+router.route('/:id').get((req,res)=>{
+    const { id } = req.params;
+    let user = users.find(u=>u.id===id)
+    res.send(`User ${user.firstName} with ${id} is found`)
+}).delete((req,res)=>{
+    const {id} = req.params;
+    users = users.filter(u=>u.id!==id)
+    res.send(`User deleted`)
+}).patch((req,res)=>{
+    const {id} = req.params
+    const{firstName, lastName, age} = req.body;
+
+    let user = users.find(u=>u.id===id)
+
+    if(firstName){
+        user.firstName = firstName
+    }
+    if(lastName){
+        user.lastName = lastName
+    }
+    if(age){
+        user.age = age
+    }
+
+    res.send(`User patched`)
+})
+
 
 module.exports = router;
